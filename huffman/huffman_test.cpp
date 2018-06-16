b0202c5cd7df8433bda6b39d7f6480f32339c277
ShSamariddin <samariddin-sharipov-99@mail.ru>
2018-06-16 04:35:26 +0300

    argument

--- huffman/huffman_test.cpp
+++ huffman/huffman_test.cpp
@@ -1,103 +1,103 @@
-#include <algorithm>
-#include <cassert>
-#include <cstdlib>
-#include <vector>
-#include <bits/stdc++.h>
-#include<fstream>
-#include "huf.h"
+//#include <algorithm>
+//#include <cassert>
+//#include <cstdlib>
+//#include <vector>
+//#include <bits/stdc++.h>
+//#include<fstream>
+//#include "huf.h"
 
-bool cheker(std::ifstream& ans1, std::ifstream& ans2){
-    const int blocksize = 10;
-    std::unique_ptr<uint8_t[]> data1(new uint8_t[blocksize]);
-    std::unique_ptr<uint8_t[]> data2(new uint8_t[blocksize]);
-    while (!ans1.eof() && !ans2.eof()){
+//bool cheker(std::ifstream& ans1, std::ifstream& ans2){
+//    const int blocksize = 10;
+//    std::unique_ptr<uint8_t[]> data1(new uint8_t[blocksize]);
+//    std::unique_ptr<uint8_t[]> data2(new uint8_t[blocksize]);
+//    while (!ans1.eof() && !ans2.eof()){
 
-        ans1.read((char*) (data1.get()), blocksize);
-        ans2.read((char*) (data2.get()), blocksize);
-        if(ans1.gcount() != ans2.gcount()){
-            return 0;
-        }
-        std::string t1 = (char*)data1.get();
-        std::string t2 = (char*) data2.get();
-        if(t1 != t2){
-            return 0;
-        }
-    }
-    if(!ans1.eof() || !ans2.eof()){
-        return 0;
-    }
-    return 1;
-}
+//        ans1.read((char*) (data1.get()), blocksize);
+//        ans2.read((char*) (data2.get()), blocksize);
+//        if(ans1.gcount() != ans2.gcount()){
+//            return 0;
+//        }
+//        std::string t1 = (char*)data1.get();
+//        std::string t2 = (char*) data2.get();
+//        if(t1 != t2){
+//            return 0;
+//        }
+//    }
+//    if(!ans1.eof() || !ans2.eof()){
+//        return 0;
+//    }
+//    return 1;
+//}
 
-int main(){
+//int main(){
 
 
-    // huf test;
-    // std::ifstream inFile1;
-    // std::ofstream outFile1;
-    // inFile1.open("./test/testenc1");
-    // outFile1.open("./test/testdec1");
-    // test.encode(inFile1, outFile1);
-    // inFile1.close();
-    // outFile1.close();
-    // std::ifstream inFile2;
-    // std::ofstream outFile2;
-    // inFile2.open("../test/testdec1");
-    // outFile2.open("../test/testsec1");
-    // test.decode(inFile2, outFile2);
-    // inFile2.close();
-    // outFile2.close();
-    // std::ifstream ans1;
-    // std::ifstream ans2;
-    // ans1.open("../test/testenc1");
-    // ans2.open("../test/testsec1");
-    // if(cheker(ans1, ans2)){
-    //     std::cout <<"YES\n";
-    // } else{
-    //     std::cout <<"NO\n";
-    // }
-    // ans1.close();
-    // ans2.close();
+//    // huf test;
+//    // std::ifstream inFile1;
+//    // std::ofstream outFile1;
+//    // inFile1.open("./test/testenc1");
+//    // outFile1.open("./test/testdec1");
+//    // test.encode(inFile1, outFile1);
+//    // inFile1.close();
+//    // outFile1.close();
+//    // std::ifstream inFile2;
+//    // std::ofstream outFile2;
+//    // inFile2.open("../test/testdec1");
+//    // outFile2.open("../test/testsec1");
+//    // test.decode(inFile2, outFile2);
+//    // inFile2.close();
+//    // outFile2.close();
+//    // std::ifstream ans1;
+//    // std::ifstream ans2;
+//    // ans1.open("../test/testenc1");
+//    // ans2.open("../test/testsec1");
+//    // if(cheker(ans1, ans2)){
+//    //     std::cout <<"YES\n";
+//    // } else{
+//    //     std::cout <<"NO\n";
+//    // }
+//    // ans1.close();
+//    // ans2.close();
 
-    // huf text;
-    // inFile1.open("/home/samariddin/Documents/cpp-course/huffman/test/testenc2");
-    // outFile1.open("/home/samariddin/Documents/cpp-course/huffman/test/testdec2");
-    // text.encode(inFile1, outFile1);
-    // inFile1.close();
-    // outFile1.close();
-    // inFile2.open("/home/samariddin/Documents/cpp-course/huffman/test/testdec2");
-    // outFile2.open("/home/samariddin/Documents/cpp-course/huffman/test/testsec2");
-    // text.decode(inFile2, outFile2);
-    // inFile2.close();
-    // outFile2.close();
-    // ans1.open("/home/samariddin/Documents/cpp-course/huffman/test/testenc2");
-    // ans2.open("/home/samariddin/Documents/cpp-course/huffman/test/testsec2");
-    // if(cheker(ans1, ans2)){
-    //     std::cout <<"YES\n";
-    // } else{
-    //     std::cout <<"NO\n";
-    // }
-    // ans1.close();
-    // ans2.close();
-    // huf text2;
-    // inFile1.open("/home/samariddin/Documents/cpp-course/huffman/test/testenc2");
-    // outFile1.open("/home/samariddin/Documents/cpp-course/huffman/test/testdec2");
-    // text2.encode(inFile1, outFile1);
-    // inFile1.close();
-    // outFile1.close();
-    // inFile2.open("/home/samariddin/Documents/cpp-course/huffman/test/testdec2");
-    // outFile2.open("/home/samariddin/Documents/cpp-course/huffman/test/testsec2");
-    // text.decode(inFile2, outFile2);
-    // inFile2.close();
-    // outFile2.close();
-    // ans1.open("/home/samariddin/Documents/cpp-course/huffman/test/testenc2");
-    // ans2.open("/home/samariddin/Documents/cpp-course/huffman/test/testsec2");
-    // if(cheker(ans1, ans2)){
-    //     std::cout <<"YES\n";
-    // } else{
-    //     std::cout <<"NO\n";
-    // }
-    // ans1.close();
-    // ans2.close();
-}
+//    // huf text;
+//    // inFile1.open("/home/samariddin/Documents/cpp-course/huffman/test/testenc2");
+//    // outFile1.open("/home/samariddin/Documents/cpp-course/huffman/test/testdec2");
+//    // text.encode(inFile1, outFile1);
+//    // inFile1.close();
+//    // outFile1.close();
+//    // inFile2.open("/home/samariddin/Documents/cpp-course/huffman/test/testdec2");
+//    // outFile2.open("/home/samariddin/Documents/cpp-course/huffman/test/testsec2");
+//    // text.decode(inFile2, outFile2);
+//    // inFile2.close();
+//    // outFile2.close();
+//    // ans1.open("/home/samariddin/Documents/cpp-course/huffman/test/testenc2");
+//    // ans2.open("/home/samariddin/Documents/cpp-course/huffman/test/testsec2");
+//    // if(cheker(ans1, ans2)){
+//    //     std::cout <<"YES\n";
+//    // } else{
+//    //     std::cout <<"NO\n";
+//    // }
+//    // ans1.close();
+//    // ans2.close();
+//    // huf text2;
+//    // inFile1.open("/home/samariddin/Documents/cpp-course/huffman/test/testenc2");
+//    // outFile1.open("/home/samariddin/Documents/cpp-course/huffman/test/testdec2");
+//    // text2.encode(inFile1, outFile1);
+//    // inFile1.close();
+//    // outFile1.close();
+//    // inFile2.open("/home/samariddin/Documents/cpp-course/huffman/test/testdec2");
+//    // outFile2.open("/home/samariddin/Documents/cpp-course/huffman/test/testsec2");
+//    // text.decode(inFile2, outFile2);
+//    // inFile2.close();
+//    // outFile2.close();
+//    // ans1.open("/home/samariddin/Documents/cpp-course/huffman/test/testenc2");
+//    // ans2.open("/home/samariddin/Documents/cpp-course/huffman/test/testsec2");
+//    // if(cheker(ans1, ans2)){
+//    //     std::cout <<"YES\n";
+//    // } else{
+//    //     std::cout <<"NO\n";
+//    // }
+//    // ans1.close();
+//    // ans2.close();
+//}
 
--- huffman/main.cpp
+++ huffman/main.cpp
@@ -1,119 +1,28 @@
-/*# include <iostream>
+# include <iostream>
 #include<string>
 #include<memory>
 #include<vector>
 # include<fstream>
 # include "huffman.h"
+# include "huf.h"
 
 int main(int argsi, char** args)
 {
 
 
     try{
-        for (int i = 0; i < argsi; i ++){
-            std::cout << args[i]<<'\n';
-        }
-        const int blocksize = 128000;
-
-        if (argsi != 5){
+        if (argsi != 4){
             throw std::runtime_error("argumetns mistake");
         }
         std::vector<std::pair<uint8_t, int> > inftree;
+        std::ifstream inFile;
+        inFile.open(args[1]);
+        std::ofstream outFile;
+        outFile.open(args[2]);
         if ((char) args[3][0] == 'e'){
-            std::ifstream inFile;
-            inFile.open(args[1]);
-            std::ofstream outFile;
-            outFile.open(args[2]);
-            HufType huft;
-
-            if (!inFile || !outFile){
-                throw std::runtime_error("wrong direction");
-            }
-
-            std::unique_ptr<uint8_t[]> data(new uint8_t[blocksize]);
-            while (!inFile.eof())
-            {
-                inFile.read((char *) (data.get()), blocksize);
-                huft.add(data.get(), inFile.gcount());
-            }
-
-            inftree = huft.info();
-            uint32_t treeSize = (uint32_t)inftree.size();
-            outFile.write((char *) &(treeSize), sizeof(int32_t));
-            for (size_t i = 0; i < inftree.size(); i ++)
-            {
-
-                outFile.write((char *) &(inftree[i].first), 1);
-                outFile.write((char *) &(inftree[i].second), sizeof(int32_t));
-            }
-            inFile.clear();
-            inFile.seekg(0);
-            Huffman ans(huft);
-            int  k = 0;
-
-            while (!inFile.eof()){
-
-                inFile.read((char*) (data.get()), blocksize);
-                std::pair<std::vector<uint8_t>, int> enc = ans.encod(data.get(), (int) inFile.gcount());
-                uint32_t siz = enc.second;
-                outFile.write((char *) &(siz), sizeof(int32_t));
-                outFile.write((char *) enc.first.data(), enc.first.size());
-            }
-            outFile.close();
-            inFile.close();
+           huf::encode(inFile, outFile);
         } else if((char) args[3][0] == 'd'){
-            std::ifstream inFile;
-            inFile.open(args[2]);
-
-            std::ofstream outFile;
-            outFile.open(args[4]);
-
-            if(!inFile || !outFile){
-                throw std::runtime_error("wrong direction");
-            }
-
-            uint32_t treeSize = 0;
-            inFile.read((char *) &(treeSize), sizeof(int32_t));
-            if(!inFile &&  treeSize > len){
-                throw std::runtime_error("wrong information in line 73");
-            }
-            for(int i = 0; i < treeSize; i ++){
-                inftree.push_back({0, 0});
-                inFile.read((char *) &(inftree[i].first), 1);
-                if(!inFile){
-                    throw std::runtime_error("wronng information in line 80");
-                }
-                inFile.read((char *) &(inftree[i].second), sizeof(int32_t));
-                if(!inFile){
-                    throw std::runtime_error("wrong information int line 84");
-                }
-
-            }
-            HufType newTree(inftree);
-            Huffman dec(newTree);
-            int k = 0;
-            std::unique_ptr<uint8_t[]> vec(new uint8_t[blocksize]);
-            while(!inFile.eof()){
-                uint32_t siz = 0;
-                inFile.read((char *) &(siz), sizeof(int32_t));
-                if(siz == 0){
-                    continue;
-                }
-                int vecSize = siz;
-                int needsize = 0;
-                if(vecSize % 8 != 0){
-                    needsize = 8 - vecSize % 8;
-                }
-                vecSize += needsize;
-                vecSize /= 8;
-                inFile.read((char *) (vec.get()), vecSize);
-                std::vector<uint8_t> res = dec.decod(vec.get(), vecSize, needsize);
-                outFile.write((char *) res.data(), res.size());
-            }
-
-            inFile.close();
-            outFile.close();
-
+           huf::decode(inFile, outFile);
         } else{
             throw std::runtime_error("wrong request");
         }
@@ -125,5 +34,5 @@ int main(int argsi, char** args)
     }
     return 0;
 
-}*/
+}
 
